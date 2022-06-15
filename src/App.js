e74dd04ae6d0fa588c8577f505c8db8198d550b0
m.fung <99631091+miafung9@users.noreply.github.com>
2022-06-14 17:07:48 -0700

    Create link container and Use state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
 import Link from './components/Link'
+import LinkContainer from './components/LinkContainer';
 
 //jsx 
 function App() {
+  const [hideLink, setHideLink] = useState(false)
+
+ const hideLinkHandler = () => {
+  setHideLink(!hideLink)
+
+ }
+ 
   return (
     <div className="App">
       <header className="App-header">
@@ -11,7 +20,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <Link/>
+        { !hideLink ? <LinkContainer/> : null}
+        <br />
+        <button onClick = {hideLinkHandler}>{hideLink ? "Hide Links" : "Show Links"}</button>
       </header>
     </div>
   );
--- src/components/Link.js
+++ src/components/Link.js
@@ -1,11 +1,15 @@
-function Link() {
+function Link({href, children}) {
+
+  return(
     <a
             className="App-link"
-            href="https://reactjs.org"
+            href={href}
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React - Modified - Finished My Feature
+            {children}
           </a>
-  
+  )
   }
+
+  export default Link
