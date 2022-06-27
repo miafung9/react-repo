adb1e64dddd0918a42e3fd002b1fc4edd422872d
m.fung <99631091+miafung9@users.noreply.github.com>
2022-06-27 16:02:06 -0700

    User Card Clone

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -6,31 +6,58 @@ import SearchUser from './SearchUser'
 
 function User() {
     const [users, setUsers] = useState([])
-    const [user, setUser] = useState (null)
-    const [userId, setUserId] = useState(0)
+    const [user, setUser] = useState(null)
+    const [userId, setUserId] = useState(1)
     const [loading, setLoading] = useState(true)
 
+    // const id = 1
+
+    console.log(user)
+
    //promises using Async/await- with arrow function
+    const getUsers = async () => {
+        try {
+            const result = await axios.get('https://jsonplaceholder.typicode.com/users')
+            setUsers(result.data)
+            setLoading(false)
+        }  catch (e){
+            console.log(e)
+            setLoading(false)
+        }
+    }
+
     const getUser = async () => {
         try {
-            const result = await axios.get("http://jsonplaceholder.typicode.com/user")
-         setUsers(result.data)
-         setLoading(false)
-        } catch (error) {
-            console.log(error)
-            setLoading(false)
-        }
-    }
-    async function getUsers(userId) {
-        try {
-            const result = await axios.get('http://jsonplaceholder.typicode.com/${user}')
+            const result = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`)
             setUser(result.data)
             setLoading(false)
-        } catch (error) {
-            console.log(error)
+        }  catch (e){
+            console.log(e)
             setLoading(false)
         }
     }
+
+
+    // const getUser = async () => {
+    //     try {
+    //         const result = await axios.get("http://jsonplaceholder.typicode.com/user")
+    //      setUsers(result.data)
+    //      setLoading(false)
+    //     } catch (error) {
+    //         console.log(error)
+    //         setLoading(false)
+    //     }
+    // }
+    // async function getUsers(userId) {
+    //     try {
+    //         const result = await axios.get('http://jsonplaceholder.typicode.com/${user}')
+    //         setUsers(result.data)
+    //         setLoading(false)
+    //     } catch (error) {
+    //         console.log(error)
+    //         setLoading(false)
+    //     }
+    // }
     //writing Async/await with normal function
     // async function getUsersNormalFunc() {
     //     try {
@@ -43,9 +70,14 @@ function User() {
     // }
     
     useEffect (() => {
-        getUser(userId)
+        getUser()
     }, [userId])
         
+        
+    useEffect (() => {
+        getUsers()
+    }, [])
+    
     // handle promises with .then() & .catch() methods
     //    const myPromise =  axios.get("http://jsonplaceholder.typicode.com/userId15")
     //     .then(result => setUsers(result.data))
@@ -55,7 +87,7 @@ function User() {
 
     // console.log("users from API: ", users)
     
-    if(loading ) {
+    if(loading) {
         return <div> LOADING...</div>}
      return <div className= 'space-y-10'>
         <SearchUser userId = {userId} setUserId= {setUserId}/>
---
+++ src/components/user/UserClone.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
+import UserCardClose from './UserCardClone'
+``
+const getUser = async () => {
+        try {
+            const result = await axios.get("http://jsonplaceholder.typicode.com/user")
+         setUsers(result.data)
+         setLoading(false)
+        } catch (error) {
+            console.log(error)
+            setLoading(false)
+        }
+    }
+    async function getUsers(userId) {
+        try {
+            const result = await axios.get('http://jsonplaceholder.typicode.com/${user}')
+            setUsers(result.data)
+            setLoading(false)
+        } catch (error) {
+            console.log(error)
+            setLoading(false)
+        }
+    }
+
+useEffect (() => {
+    getUser()
+    getUsers()
+}, [])
+
+if(loading) {
+    return <div> LOADING...</div>
+return <div className= 'space-y-10'>
+    <SearchUser userId = {userId} setUserId = {setUserId} />
+    {user &&
+        <UserCard user= {user} />
+        }
+</div>
+}
+ export default UserClone
